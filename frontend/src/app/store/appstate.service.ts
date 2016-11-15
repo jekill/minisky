058d508c78d5cd2f912fb260e50d4b621d317e4c
Evgeny Fomin <info@jeka.ru>
2016-11-15 17:07:10 +0300

    add redux localstorage

--- frontend/src/app/store/appstate.service.ts
+++ frontend/src/app/store/appstate.service.ts
@@ -3,6 +3,10 @@ import {IAppState} from "./state";
 import {Injectable} from "@angular/core";
 import {rootReducer} from "./reducers";
 import {Languages} from "../translate/languages";
+import {compose} from "redux";
+
+const persistState = require('redux-localstorage');
+
 
 @Injectable()
 export class AppStateService {
@@ -10,6 +14,19 @@ export class AppStateService {
     }
 
     configure() {
-        this.store.configureStore(rootReducer, {uiLang: Languages.ru, isAuthenticated: false});
+        this.store.configureStore(
+            rootReducer,
+            {uiLang: Languages.ru, isAuthenticated: false},
+            null,
+            this.enchacers()
+        );
+    }
+
+    private enchacers() {
+        const enhancers = compose(
+            persistState(/*paths, config*/),
+        );
+
+        return enhancers;
     }
 }
